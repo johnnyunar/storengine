14adc9dc79097eafd7ccb79d1333b589444e68a0
Jan Unar <johnunar@gmail.com>
2022-06-27 18:54:51 +0200

    Fixed GoPay callback error

--- shop/views.py
+++ shop/views.py
@@ -177,15 +177,16 @@ class PaymentCallbackView(View):
         payment_id = request.GET.get("id")
         if order and payment_id:
             payment_details = get_gopay_payment_details(payment_id)
-            gopay_payment, _created = GopayPayment.objects.update_or_create(
+            gopay_payment, created = GopayPayment.objects.update_or_create(
                 payment_id=payment_id,
                 defaults={
                     "payment_status": payment_details["state"],
                     "payment_data": payment_details,
                 },
             )
-            order.gopay_payment = gopay_payment
-            order.save()
+            if created:
+                order.gopay_payment = gopay_payment
+                order.save()
             if order.is_paid:
                 return HttpResponseRedirect(reverse_lazy("shop:thank_you_paid"))
             else:
